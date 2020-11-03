c9fbcd431a5233031badb5243b8e7c97a8dbeb16
mattmarnien <mgm215@gmail.com>
2020-11-03 12:51:51 -0500

    readded first-dotnet

---
+++ first-dotnet/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // loop over the args from command line
+        for(int ii = 0; ii < args.Length; ii++)
+        {
+        string thisArg = args[ii];
+        System.Console.WriteLine(thisArg);
+        }
+        
+        System.Console.WriteLine("Also. Hey there world. What up.");
+    }
+
+}
