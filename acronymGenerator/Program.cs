e1fa37bf0f9b41a5547fe055007493b49b10f956
mattmarnien <mgm215@gmail.com>
2020-11-04 10:50:39 -0500

    pushing change to acronym gen

--- acronymGenerator/Program.cs
+++ acronymGenerator/Program.cs
@@ -3,19 +3,20 @@
 namespace acronymGenerator
 {
     class Program
-    {
-        static void Main(string[] args)
+    {       
+     static void Main(string[] args)
         {
             Console.WriteLine("Please enter a term.");
             string term = Console.ReadLine();
             string[] words = term.Split(' ');
    
             foreach(var word in words){
-             
+                if(word == "and" || word == "the"){
+                    continue;
+                }
                 string capitalWord = word.ToUpper();
                 Console.Write($"{capitalWord[0]}.");
             }
-
         }
     }
 }
